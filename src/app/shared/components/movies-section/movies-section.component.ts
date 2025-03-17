84e3f298915e155ce179f458fd979e26d84d0beb
Juan Carlos <JC_ORION_27@HOTMAIL.COM>
2025-03-17 16:30:00 -0600

    se genera la estructura de la app, y la carpeta scss de estilos

---
+++ src/app/shared/components/movies-section/movies-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-movies-section',
+  imports: [],
+  templateUrl: './movies-section.component.html',
+  styleUrl: './movies-section.component.scss'
+})
+export class MoviesSectionComponent {
+
+}
