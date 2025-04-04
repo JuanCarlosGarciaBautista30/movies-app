508bef55e26ca2cf2a12663c91cdb9b20b6681c6
Juan Carlos <JC_ORION_27@HOTMAIL.COM>
2025-04-03 21:55:58 -0600

    se crea el directorio components en pages y se mueven los componentes movies-section right-side y side-menu

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { SideMenuComponent } from "../../shared/components/side-menu/side-menu.component";
-import { MoviesSectionComponent } from "../../shared/components/movies-section/movies-section.component";
-import { RightSideComponent } from "../../shared/components/right-side/right-side.component";
+import { SideMenuComponent } from '../components/side-menu/side-menu.component';
+import { MoviesSectionComponent } from '../components/movies-section/movies-section.component';
+import { RightSideComponent } from '../components/right-side/right-side.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [SideMenuComponent, MoviesSectionComponent, RightSideComponent],
+  imports: [
+
+    SideMenuComponent,
+    MoviesSectionComponent,
+    RightSideComponent,
+    SideMenuComponent,
+    MoviesSectionComponent,
+    RightSideComponent
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
